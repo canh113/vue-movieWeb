be4fbd8d337c23aed29242d7a020120d74f232bb
CanhLe <minhcanh2411@gmail.com>
2022-05-14 23:34:56 +0700

    update

--- vue-movie-web/src/components/ListMovie.vue
+++ vue-movie-web/src/components/ListMovie.vue
@@ -13,13 +13,29 @@
       <tbody>
         <tr v-for="(item, index) in requestlist" :key="item.Id">
           <th scope="row">{{ index + 1 }}</th>
-          <td>{{ item.name }}</td>
+          <td class="pointer" @click="getDataById(item)">{{ item.name }}</td>
           <td>{{ item.genre }}</td>
           <td>{{ item.duration }}</td>
           <td>{{ $moment(item.releaseDate).format("DD/MM/yyyy") }}</td>
         </tr>
       </tbody>
     </table>
+    <div v-if="isEdit">
+      <h4>Change data list</h4>
+      <div class="mb-3">
+        <label class="form-label">Name</label>
+        <input type="text" class="form-control" v-model="name" />
+      </div>
+      <div class="mb-3">
+        <label class="form-label">Genre</label>
+        <input type="text" class="form-control" v-model="genre" />
+      </div>
+      <div class="mb-3">
+        <label class="form-label">Duration</label>
+        <input type="text" class="form-control" v-model="duration" />
+      </div>
+      <button @click="saveData()" class="btn btn-primary">Submit</button>
+    </div>
   </div>
 </template>
 
@@ -29,6 +45,13 @@ export default {
   data() {
     return {
       requestlist: [],
+      requestInfo: [],
+      isEdit: false,
+      name: null,
+      genre: null,
+      duration: null,
+      id: null,
+      loadListMovie: false,
     };
   },
   mounted() {
@@ -38,14 +61,62 @@ export default {
     loadList() {
       this.getRequest();
     },
+
+    loadListMovie() {
+      this.getRequest();
+    },
   },
   methods: {
     async getRequest() {
       let dataT = await this.$http.get("/movies");
       this.requestlist = dataT.data;
     },
+
+    async getDataById(e) {
+      this.isEdit = true;
+      let requestInfo = e;
+      this.requestInfo = requestInfo;
+      this.name = e.name;
+      this.genre = e.genre;
+      this.duration = e.duration;
+    },
+    async saveData() {
+      //put
+      let data = await this.$http.put(
+        `/movies/${this.requestInfo.id}`,
+        {
+          Id: this.requestInfo.id,
+          Name: this.name,
+          Genre: this.genre,
+          Duration: this.duration,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      if (data && data.status == 204) {
+        this.loadListMovie = true;
+        this.isEdit = false;
+        alert("Your request update successfully!");
+        // clear form
+        this.clearForm();
+      } else {
+        alert("Your request update fail!");
+      }
+    },
+    clearForm() {
+      this.name = null;
+      this.genre = null;
+      this.duration = null;
+    },
   },
 };
 </script>
 
-<style></style>
+<style>
+.pointer {
+  cursor: pointer;
+}
+</style>
